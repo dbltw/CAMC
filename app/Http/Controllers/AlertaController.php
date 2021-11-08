92140bd495d5f996d035060bb46b438fb0845c9f
LTW <LTW@DBARRANC.home>
2021-11-08 12:36:21 +0100

    sdf

--- app/Http/Controllers/AlertaController.php
+++ app/Http/Controllers/AlertaController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
-use App\Alerta;
+use App\Alerta; asdfasdf
 
 class AlertaController extends Controller
 {
